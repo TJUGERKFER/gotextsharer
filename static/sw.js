d8b166fd9532d6c10fedfebe57e7ba5bf5908f98
unknown <TJUGERKFER>
2023-12-15 17:07:22 +0800

    新增 也许实现了PWA功能

---
+++ static/sw.js
@@ -0,0 +1,16 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+  };
+  
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      addResourcesToCache([
+        "/",
+        "/index.html",
+        "/mdui.min.css",
+        "/mdui.min.js",
+        "/admin/index.html",
+      ]),
+    );
+  });
