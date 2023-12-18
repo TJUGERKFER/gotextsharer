2ae6f7a4971f2eab9d70fa9646c0ed6d51ece2e6
unknown <TJUGERKFER>
2023-12-18 18:51:18 +0800

    改进 也许让pwa工作得更正常了

--- static/sw.js
+++ static/sw.js
@@ -6,11 +6,14 @@ const addResourcesToCache = async (resources) => {
   self.addEventListener("install", (event) => {
     event.waitUntil(
       addResourcesToCache([
-        "/",
         "/index.html",
         "/css/mdui.min.css",
         "/js/mdui.min.js",
+        "/js/changebrightness.js",
         "/admin/index.html",
+        "/icons/material-icons/MaterialIcons-Regular.ijmap",
+        "/icons/material-icons/MaterialIcons-Regular.woff",
+        "/icons/material-icons/MaterialIcons-Regular.woff2"
       ]),
     );
   });
